07794d9f38b9109d2db319d91b54f5fdb450244f
ofer3k <67784636+ofer3k@users.noreply.github.com>
2021-04-20 17:50:12 +0300

    fetching and posting urls from mongo

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,7 @@
 const Post=require('../models/post')
 const slugify=require('slugify')
 const pupeteer=require('puppeteer')
+const cheerio=require('cheerio')
 
 exports.create=(req,res)=>{
 //    console.log(req.body)
@@ -61,8 +62,25 @@ exports.remove=(req,res)=>{
         })
     })
 }
+
+exports.removeAll=async(req,res)=>{
+    // const {slug}=req.params
+    try {
+        await Post.deleteMany();
+        console.log('All Data successfully deleted');
+      } catch (err) {
+        console.log(err);
+      }
+      res.send('all was deleted')
+}
+
+
 exports.crawl=async (req,res)=>{
     const {title,content,user}=req.body
+    let urls=['/in/stefanhyltoft']
+    
+    // let linksToVisit
+    let visitedLinks=[]
     // launching browser
     const browser=await pupeteer.launch({
         headless:false,
@@ -74,11 +92,64 @@ exports.crawl=async (req,res)=>{
     // insert auth of user
     await page.type('#username','ofer3klein@gmail.com')
     await page.type('#password','ofer3k1998')
+    
     await page.click('.btn__primary--large.from__button--floating')
-    sleep(3000)
-    await page.goto('https://www.linkedin.com/in/stefanhyltoft')
-
-    console.log(title,content,user)
+    await sleep(3200)
+    while(urls.length>0){
+        // fetch all url from db
+        // await Post.find({}).exec((err,posts)=>{
+        //     if(err)console.log(err)
+        //     // sort and map the urls
+        //     urls=posts.sort(function(a,b){return a.createdAt - b.createdAt}).map((el)=>el.title) 
+        // })
+        // 
+        const currentUrl=urls.pop()
+        if(visitedLinks.includes(currentUrl))continue
+        await page.goto('https://www.linkedin.com'+currentUrl)
+        await sleep(5000)
+        const html =await page.content()
+        const $=cheerio.load(html)
+        const newLinkToVisit=$('.pv-browsemap-section__member').map((index,element)=>$(element).attr('href')).get()
+        // console.log(linksToVisit)
+        newLinkToVisit.forEach(async(el)=>{
+            let title,content,user ;//req.params
+            title=el
+            content='121'
+            const slug=slugify(title)
+// validate
+switch(true){
+    case !title:
+        console.log('title is required!')
+        break;
+    case !content:
+        console.log('content is required!')
+        break;
+}
+// create
+await Post.create({title,content,user,slug},(err,post)=>{
+    if(err){
+        console.log(err)
+        // res.status(400).json({error:'Duplicate post. try another title'})
+    }
+    else{
+        console.log('url:',post.title)
+    }
+    // res.json(post)
+})
+        })
+        // linksToVisit=[...linksToVisit,...newLinkToVisit]
+        visitedLinks.push(currentUrl)
+        // here
+        await Post.find({}).exec((err,posts)=>{
+            if(err)console.log(err)
+            // sort and map the urls
+            urls=posts.sort(function(a,b){return a.createdAt - b.createdAt}).map((el)=>el.title) 
+        })
+        // 
+        await sleep(2000)
+    }
+    
+    // console.log(title,content,user)
     res.json({
         status:'200'
     })
@@ -86,3 +157,4 @@ exports.crawl=async (req,res)=>{
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
   }
+  
--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@ const express=require('express')
 const router = require('express').Router(); 
 
 // import controllers methods
-const {create,list,read,update,remove,crawl}=require('../controllers/post');
+const {create,list,read,update,remove,crawl, removeAll}=require('../controllers/post');
 
 router.get('/crawl',crawl)
 router.post('/post',create)
@@ -10,5 +10,6 @@ router.get('/posts',list)
 router.get('/posts/:slug',read)
 router.put('/post/:slug',update)
 router.delete('/posts/:slug',remove)
+router.delete('/postsall',removeAll)
 
 module.exports=router
